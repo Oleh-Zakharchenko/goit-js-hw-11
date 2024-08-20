15809ed74c9160f2581a7eabdfd215d6fdc88b0c
Oleh Zakharchenko <164777305+Oleh-Zakharchenko@users.noreply.github.com>
2024-08-20 22:18:03 +0200

    first_fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,40 +1,11 @@
-import iziToast from "izitoast";
-
-import "izitoast/dist/css/iziToast.min.css";
-
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { createGalleryMarkup } from './render-functions.js';
-
 const API_KEY = '45522056-70580155eac140dfe7225b3e6';
 
 export function fetchImages(query) {
-    const loader = document.querySelector('.loader');
-    loader.style.display = 'block';
-    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`)
+    return fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to fetch images');
             }
             return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            loader.style.display = 'none';
-            if (data.hits.length === 0) {
-                iziToast.info({
-                    title: 'Info',
-                    message: 'Sorry, there are no images matching your search query. Please try again!'
-                });
-            } else {
-                createGalleryMarkup(data.hits);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching images:', error);
-            loader.style.display = 'none';
-            iziToast.error({
-                title: 'Error',
-                message: 'Failed to fetch images. Please try again later.'
-            });
         });
 }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,45 @@
 import { fetchImages } from './js/pixabay-api';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
+import { createGalleryMarkup } from './js/render-functions.js';
 
 const form = document.querySelector('#search-form');
 const input = document.querySelector('#search-input');
+const loader = document.querySelector('.loader');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const query = input.value.trim();
-    if (!query) {
-        iziToast.warning({
-            title: 'Warning',
-            message: 'Please enter a search query',
+  event.preventDefault();
+  const query = input.value.trim();
+  
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query',
+    });
+    return;
+  }
+  
+  loader.style.display = 'block';
+  fetchImages(query)
+    .then(data => {
+      loader.style.display = 'none';
+      if (data.hits.length === 0) {
+        iziToast.info({
+          title: 'Info',
+          message: 'Sorry, there are no images matching your search query. Please try again!'
         });
-        return;
-    }
-    fetchImages(query);
-    input.value = '';
+      } else {
+        createGalleryMarkup(data.hits);
+      }
+    })
+    .catch(error => {
+      loader.style.display = 'none';
+      console.error('Error fetching images:', error);
+      iziToast.error({
+        title: 'Error',
+        message: 'Failed to fetch images. Please try again later.'
+      });
+    });
+  
+  input.value = '';
 });
